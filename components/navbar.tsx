9d55d3fa0f00c2713ff8e57719faa03930e552ea
ndela-ntu <ntulilindelani4@gmail.com>
2025-04-25 23:51:27 +0200

    was garbage

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -187,8 +187,8 @@ const Navbar = () => {
 
           {/* Mobile Menu (unchanged) */}
           <div
-            className={`mobile-menu fixed inset-y-0 left-0 w-64 bg-white shadow-lg transform ${
-              isMenuOpen ? "translate-x-0" : "-translate-y-full"
+            className={`mobile-menu fixed inset-y-0 left-0 w-64 bg-white transform ${
+              isMenuOpen ? "translate-x-0" : "-translate-x-full"
             } md:hidden transition-transform duration-300 ease-in-out z-50`}
           >
             <div className="flex flex-col p-4 space-y-4 mt-8">
