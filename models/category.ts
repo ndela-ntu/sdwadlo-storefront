310e0982c847b5c7f058a9810c95fdd9b9acb5f5
ndela-ntu <ntulilindelani4@gmail.com>
2025-05-13 17:28:08 +0200

    added type to ITag and ICategory

--- models/category.ts
+++ models/category.ts
@@ -4,5 +4,6 @@ export default interface ICategory {
     id: number;
     name: string;
     media_url: string;
+    type: 'Category';
     status: "Active" | "Inactive";
 }
--- models/tag.ts
+++ models/tag.ts
@@ -2,5 +2,6 @@ export default interface ITag {
   id: number;
   name: string;
   media_url: string;
+  type: 'Tag'
   status: "Active" | "Inactive";
 }
